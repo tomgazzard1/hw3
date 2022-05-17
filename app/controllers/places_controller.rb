6711f5ecc086313ab5096093f461efda194cab29
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-05-17 22:53:34 +0000

    Resources, view for places and places controller

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,22 @@
+class PlacesController < ApplicationController
+
+  def index # Index is used to read (display) a list of places (HTTP Method: GET)
+    @place = Place.all #Why is this place and not places? Are classes singular? Where is the class defined and how?
+  end
+
+  def show # Show is used to read (display) info for a single place (HTTP Method: GET)
+    @place = Place.find_by({ "id" => params["id"] })
+  end
+
+  def new # New is a form to fill out about a new place (HTTP Method: GET)
+    @place = Place.new
+  end
+
+  def create # Receives information from the form and creates the place (HTTP Method: POST)
+    @place = Company.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources "places"
+  resources "posts"
+  # get("/places", { :controller => "places", :action => "index" })
+  # get("/posts", { :controller => "posts", :action => "index" })
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
