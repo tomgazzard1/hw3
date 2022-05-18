b652ee4d820f57df55d04330261bd7bc93ed2d3d
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-05-18 15:59:33 +0000

    Added some notes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,8 +5,8 @@ class PlacesController < ApplicationController
   end
 
   def show # Show is used to read (display) info for a single place (HTTP Method: GET)
-    @place = Place.find_by({"id" => params["id"]})
-    @posts = Post.where({"place_id" => @place["id"]})
+    @place = Place.find_by({"id" => params["id"]}) # Takes the parameter on the show page. From console: Parameters: {"id"=>"1"}
+    @posts = Post.where({"place_id" => @place["id"]}) # Takes the value from the line above
   end
 
   def new # New is a form to fill out about a new place (HTTP Method: GET)
@@ -15,7 +15,8 @@ class PlacesController < ApplicationController
 
   def create # Receives information from the form and creates the place (HTTP Method: POST)
     @place = Place.new
-    @place["name"] = params["place"]["name"]
+    @place["name"] = params["place"]["name"] # Comes from the POST /places. Hash within a hash. From console: Parameters: {"authenticity_token"=>"[FILTERED]", "place"=>{"name"=>"Toronto"}, "commit"=>"Create Place"}
+    # Question: Does this save to the DB as the "name" field in the places table?
     @place.save
     redirect_to "/places"
   end
